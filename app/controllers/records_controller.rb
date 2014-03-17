d94f9a13a6f0a2004df2fbdd23d2ed9ca32d36bb
Max Woolf <maximus@zingysaturn.co.uk>
2014-03-17 19:08:07 +0000

    silly bug

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,7 +2,7 @@ class RecordsController < ApplicationController
 
   respond_to :json
   def index
-    @records = Record.where(authority: params[:authority].capitalize).last 8
+    @records = Record.where(authority: params[:authority]).last 8
     render json: @records.to_json(except: [:updated_at])
   end
 
