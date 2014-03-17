16820fc6f26eea9e7cd6a5161c10eb5c6628db39
Max Woolf <maximus@zingysaturn.co.uk>
2014-03-17 17:34:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BloodStocks::Application.config.secret_key_base = '249a6dbf5b6231bdd51869981d7913b9a282cf7c9f5a8f30a81b717c454db6f69cedf171c318ddc582adabbbb65c5624b37a82603caff58bf27580a33cf9a812'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BloodStocks::Application.config.session_store :cookie_store, key: '_BloodStocks_session'
