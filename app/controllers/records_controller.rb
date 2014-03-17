37effaeac0040cbf2134d1e159d8999507dd69cf
Max Woolf <maximus@zingysaturn.co.uk>
2014-03-17 18:53:25 +0000

    get last few

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,8 +2,8 @@ class RecordsController < ApplicationController
 
   respond_to :json
   def index
-    @records = Record.where(authority: params[:authority].capitalize)
-    render json: @records.to_json(only: [:blood_type_id, :days_remaining, :authority])
+    @records = Record.where(authority: params[:authority].capitalize).last 8
+    render json: @records.to_json(except: [:updated_at])
   end
 
 end
