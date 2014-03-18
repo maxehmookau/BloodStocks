c4603fb146165c3020bd60377911877ef4698e9c
Max Woolf <maximus@zingysaturn.co.uk>
2014-03-18 19:58:00 +0000

    adds historical data

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,7 +2,11 @@ class RecordsController < ApplicationController
 
   respond_to :json
   def index
-    @records = Record.where(authority: params[:authority]).last 8
+    if params[:all] == 'true'
+      @records = Record.where(authority: params[:authority])
+    else
+      @records = Record.where(authority: params[:authority]).last 8
+    end
     render json: @records.to_json(except: [:updated_at])
   end
 
